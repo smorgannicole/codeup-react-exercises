c2a7a82b1c9c83c2be984053f6a384cd32342366
Morgan <smorgannicole@gmail.com>
2023-12-13 17:06:32 -0600

    react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import "./components/Header/style.css";
+import Index from "./components/Header";
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <Index />
+      <main>
+      </main>
+      <footer>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+const Index = () => {
+    return (
+        <header>
+            <h1>header</h1>
+        </header>
+    );
+}
+export default Index;
